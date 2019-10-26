3762c1b7e9d266f274b3f7f5d391bfdf5646b5eb
Mario Alberto Barragán Espinosa <>
2019-10-26 12:19:41 -0500

    Adding to a list of words

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Mario Alberto Barragán Espinosa on 26/10/19.
+//  Copyright © 2019 Mario Alberto Barragán Espinosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .autocapitalization(.none)
+
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+        }
+    }
+    
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // exit if the remaining string is empty
+        guard answer.count > 0 else {
+            return
+        }
+
+        // extra validation to come
+
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
